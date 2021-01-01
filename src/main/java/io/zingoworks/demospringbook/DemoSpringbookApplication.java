a340bde78c0b5365f6072a9940e118fe6f6e4f43
zingoworks <zingoworks@gmail.com>
2021-01-01 20:18:51 +0900

    Init project

---
+++ src/main/java/io/zingoworks/demospringbook/DemoSpringbookApplication.java
@@ -0,0 +1,13 @@
+package io.zingoworks.demospringbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zingoworks/demospringbook/DemoSpringbookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zingoworks.demospringbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
