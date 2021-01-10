000d654e7893fcf05c181f1b27e21b87206cad62
zingoworks <zingoworks@gmail.com>
2021-01-10 12:39:22 +0900

    Setter를 통한 의존관계 주입

--- src/main/java/io/zingoworks/demospringbook/user/dao/UserDao.java
+++ src/main/java/io/zingoworks/demospringbook/user/dao/UserDao.java
@@ -11,15 +11,20 @@ public class UserDao {
 	
 	private ConnectionMaker connectionMaker;
 	
-	public UserDao(ConnectionMaker connectionMaker) {
+//	public UserDao(ConnectionMaker connectionMaker) {
+//		this.connectionMaker = connectionMaker;
+//	}
+	
+	// setter를 통한 의존성 주입
+	public void setConnectionMaker(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
 	
-	public UserDao() {
-		// DaoFactory를 통한 의존관계 검색
-		CountingDaoFactory countingDaoFactory = new CountingDaoFactory();
-		this.connectionMaker = countingDaoFactory.connectionMaker();
-	}
+//	public UserDao() {
+//		// DaoFactory를 통한 의존관계 검색
+//		CountingDaoFactory countingDaoFactory = new CountingDaoFactory();
+//		this.connectionMaker = countingDaoFactory.connectionMaker();
+//	}
 	
 //	public UserDao() {
 //		// 스프링의 IoC 컨테이너(애플리케이션 컨텍스트)를 통한 의존관계 검색
