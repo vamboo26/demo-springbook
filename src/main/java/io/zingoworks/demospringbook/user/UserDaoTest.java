bba1f250a6dabdaf9b9dcaa0546280ed3f12bbc9
zingoworks <zingoworks@gmail.com>
2021-01-11 22:54:08 +0900

    2.2.1 테스트 검증의 자동화

--- src/main/java/io/zingoworks/demospringbook/user/UserDaoTest.java
+++ src/main/java/io/zingoworks/demospringbook/user/UserDaoTest.java
@@ -1,6 +1,5 @@
 package io.zingoworks.demospringbook.user;
 
-import io.zingoworks.demospringbook.user.dao.CountingConnectionMaker;
 import io.zingoworks.demospringbook.user.dao.UserDao;
 import io.zingoworks.demospringbook.user.domain.User;
 import lombok.extern.slf4j.Slf4j;
@@ -13,16 +12,10 @@ import java.time.LocalTime;
 @Slf4j
 public class UserDaoTest {
 	
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+	public static void main(String[] args) throws SQLException {
 		ApplicationContext applicationContext = new GenericXmlApplicationContext("application-context.xml");
 		
 		UserDao dao = applicationContext.getBean("userDao", UserDao.class);
-		UserDao dao2 = applicationContext.getBean("userDao", UserDao.class);
-		UserDao dao3 = applicationContext.getBean("userDao", UserDao.class);
-		
-		System.out.println("dao2 = " + dao2);
-		System.out.println("dao3 = " + dao3);
 		
 		User user = new User();
 		user.setId(String.valueOf(LocalTime.now()).substring(0, 10));
@@ -31,20 +24,13 @@ public class UserDaoTest {
 		
 		dao.add(user);
 		
-		log.info("{} 등록 성공", user.getId());
-		
 		User user2 = dao.get(user.getId());
-		log.debug("이름:{}", user2.getName());
-		log.debug("암호:{}", user2.getPassword());
-		log.debug("{} 조회 성공", user2.getId());
-		
-		dao.get(user.getId());
-		dao.get(user.getId());
-		dao.get(user.getId());
-		dao.get(user.getId());
-		dao.get(user.getId());
-		
-		CountingConnectionMaker ccm = applicationContext.getBean("connectionMaker", CountingConnectionMaker.class);
-		System.out.println("ccm.getCounter() = " + ccm.getCounter());
+		if (!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 (name)");
+		} else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 (password)");
+		} else {
+			System.out.println("조회 테스트 성공");
+		}
 	}
 }
